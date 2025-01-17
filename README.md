e0bed2fbac340ccd26676c012fdae963c7c24e58
djrogers2019 <rogersjdouglas@gmail.com>
2025-01-17 07:47:43 -0600

    Added the final touches and an update note

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # bookbot
 Bookbot is my first project I worked on!
+This project has been completed as of 01/16
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ def main():
     book_path = "github.com/DJROGERS2019/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    num_char = get_num_characters(text)
     print(f"{num_words} words found in the document")
     
 
